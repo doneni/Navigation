87681ffe2c3d38a75781d908d298eb96f9c24028
doneni <xxerrenn@icloud.com>
2022-08-29 13:59:58 +0900

    hi

---
+++ App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, TouchableOpacity, Button } from 'react-native';
+import 'react-native-gesture-handler';
+import { createAppContainer, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const Home = () =>
+    <View style={styles.eachView}>
+        <Text> 홈 화면 입니다. </Text>
+        <Button
+          title="챗 화면으로 가기"
+          onPress={() => this.props.navigation.navigate('Chat')}
+        />
+        <Button
+          title="세팅 화면으로 가기"
+          onPress={() => this.props.navigation.navigate('Settings')}
+        />
+    </View>
+
+const Chat = () =>
+    <View style={styles.eachView}>
+        <Text> 챗 화면 입니다. </Text>
+    </View>
+
+const Settings = () =>
+    <View style={styles.eachView}>
+        <Text> 세팅 화면 입니다. </Text>
+    </View>
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Chat" component={Chat} />
+                <Stack.Screen name="Settings" component={Settings} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  centerView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  eachView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+})
